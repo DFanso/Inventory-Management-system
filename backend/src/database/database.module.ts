da60bcbdcf6da88cb901b60595c607604c2b2019
Leo Gavin <leogavin123@outlook.com>
2024-07-23 13:21:00 +0530

    k8s setup

--- backend/src/database/database.module.ts
+++ backend/src/database/database.module.ts
@@ -1,22 +1,31 @@
 import { Module } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseInitService } from './database-init.service';
 
 @Module({
   imports: [
+    ConfigModule,
     TypeOrmModule.forRootAsync({
-      useFactory: (configService: ConfigService) => ({
-        type: 'mysql',
-        host: configService.getOrThrow('MYSQL_HOST'),
-        port: configService.getOrThrow('MYSQL_PORT'),
-        database: configService.getOrThrow('MYSQL_DATABASE'),
-        username: configService.getOrThrow('MYSQL_USERNAME'),
-        password: configService.getOrThrow('MYSQL_PASSWORD'),
-        autoLoadEntities: true,
-        synchronize: configService.getOrThrow('MYSQL_SYNCHRONIZE'),
-      }),
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const dbInitService = new DatabaseInitService(configService);
+        await dbInitService.onModuleInit();
+        return {
+          type: 'mysql',
+          host: configService.get('MYSQL_HOST'),
+          port: configService.get('MYSQL_PORT'),
+          username: configService.get('MYSQL_USERNAME'),
+          password: configService.get('MYSQL_PASSWORD'),
+          database: configService.get('MYSQL_DATABASE'),
+          autoLoadEntities: true,
+          synchronize: configService.get('MYSQL_SYNCHRONIZE'),
+        };
+      },
       inject: [ConfigService],
     }),
   ],
+  providers: [DatabaseInitService],
+  exports: [DatabaseInitService],
 })
 export class DatabaseModule {}
